1f9244d97af1e7c7a9cbf1658cc3fa1da87b7976
Serge Sukhanov <ssuhanov@icloud.com>
2017-09-16 14:37:12 +0300

    Add a test for model

---
+++ WeatherServiceTests/Model/CityWeatherTests.swift
@@ -0,0 +1,30 @@
+//
+//  CityWeatherTests.swift
+//  WeatherService
+//
+//  Created by Serge Sukhanov on 9/16/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherService
+
+class CityWeatherTests: XCTestCase {
+    
+    var instance: CityWeather!
+    let mainDictResult: JSONDictonary = ["temp" : 23.5 as AnyObject]
+    var dictResultWithName: JSONDictonary = ["name": "Moncton" as AnyObject]
+    let dictResultWithoutName: JSONDictonary = ["notName" : "Moncton" as AnyObject]
+    
+    override func setUp() {
+        super.setUp()
+        self.dictResultWithName["main"] = self.mainDictResult as AnyObject
+    }
+    
+    func testNameUpdatesCorrectly() {
+        self.instance = CityWeather(from: self.dictResultWithName)
+        XCTAssertEqual(self.instance.city, "Moncton")
+        XCTAssertEqual(self.instance.temperature, 23.5)
+    }
+    
+}
